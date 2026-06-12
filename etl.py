07083499d1701c1fb84ff07c4f09f747348ac5f0
Noniewiemwlasnie <siuzdakjakub55@gmail.com>
2026-06-12 16:57:26 +0200

    zmiana zakresu czasowego

--- etl.py
+++ etl.py
@@ -160,7 +160,7 @@ def run_flight_etl():
     logging.info("Starting FLIGHT ETL")
     with connect_db() as conn:
         try:
-            end_ts = int(datetime.now(timezone.utc).timestamp()) - (24*3600)
+            end_ts = int(datetime.now(timezone.utc).timestamp()) - (2*3600)
             begin_ts = end_ts - 12 * 3600
             flights = fetch_all_flights(begin_ts, end_ts)
             if not flights:
